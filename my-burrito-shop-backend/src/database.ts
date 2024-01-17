9381335f5e0a3b2f52693a31d94ed0c3e41d85cd
ris0801 <rbs7261@nyu.edu>
2024-01-16 20:22:02 -0500

    Updating dockerfiles and dependecies file

--- my-burrito-shop-backend/src/database.ts
+++ my-burrito-shop-backend/src/database.ts
@@ -1,11 +1,23 @@
+// import mysql from 'mysql2';
+
+// const connection = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'my-secret-pw',
+//   database: 'burrito_shop', 
+//   port: 3308
+// });
+
+// export default connection;
+
 import mysql from 'mysql2';
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: 'my-mysql-network', // Name of your MySQL Docker container
   user: 'root',
-  password: 'my-secret-pw',
+  password: 'mypassword', // Your MySQL root password
   database: 'burrito_shop', 
-  port: 3308
+  port: 3306 // Make sure this port matches the MySQL container's port
 });
 
 export default connection;
